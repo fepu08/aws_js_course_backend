abd659027047ed0fb84eeebcce014967c8d47211
fepu08 <ferencbesenyei@protonmail.com>
2023-10-09 10:29:31 +0200

    removed uuid because it caused error with getProductById

--- product-service/mocks/products.mock.ts
+++ product-service/mocks/products.mock.ts
@@ -1,44 +1,43 @@
 import { Product } from 'models/product.types';
-import { v4 as uuid } from 'uuid';
 
 export const mockProducts: Product[] = [
   {
-    id: uuid(),
+    id: '99818adf-8689-4612-843c-18027d71954c',
     title: "Assassin's Creed Mirage",
     description: 'A new year a new AC',
     price: 70,
     count: 111,
   },
   {
-    id: uuid(),
+    id: '6b7a91da-9482-4237-bf84-be20781df4cb',
     title: 'Factorio',
     description: "Let's build a Factory",
     price: 32,
     count: 26,
   },
   {
-    id: uuid(),
+    id: 'aee23242-2f8b-4aa3-935c-bb63059a0710',
     title: 'Call of Duty: Modern Warfare 3',
     description: 'A new year a new COD',
     price: 70,
     count: 1234,
   },
   {
-    id: uuid(),
+    id: '2d9634ab-e7c1-4ebb-87a5-a50e21183d3e"',
     title: "Baldur's Gate 3",
     description: 'GOTY',
     price: 72,
     count: 152,
   },
   {
-    id: uuid(),
+    id: '710dfbbe-fb00-45dc-aaf4-c92b8dd00e7d',
     title: 'Days Gone',
     description: 'Motorcycle && Zombies === 🔥',
     price: 30,
     count: 86,
   },
   {
-    id: uuid(),
+    id: '710dfbbe-fb00-45dc-aaf4-c92b8dd00e7d',
     title: 'Lies of P',
     description: 'Soulslike Pinocchio',
     price: 60,
